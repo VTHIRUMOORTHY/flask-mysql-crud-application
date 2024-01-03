a63970374e5d8427b215da69c7618e07edc8d016
Vikneshkumar Thirumoorthy <vikneshkumar18@gmail.com>
2024-01-03 13:10:27 -0600

    Workorder related changes

--- crud.sql
+++ crud.sql
@@ -4,21 +4,21 @@ SET time_zone = "+00:00";
 
 use crud;
 
-CREATE TABLE `students` (
+CREATE TABLE `workorders` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `students` (`id`, `name`, `email`, `phone`) VALUES
+INSERT INTO `workorders` (`id`, `name`, `email`, `phone`) VALUES
 (1, 'John Doe', 'johndoe@gmail.com', '999999999'),
 (2, 'Viknesh Thirumoorthy', 'viknesh.thirumoorthy', '3124839375'),
 (3, 'Shanmugavel KC', 'kc.shanmugavel@gmail.com', '3128880270'),
 (4, 'Pavithra KC', 'pavi.kc222@gmail.com', '3129564700');
 
-ALTER TABLE `students`
+ALTER TABLE `workorders`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+ALTER TABLE `workorders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
